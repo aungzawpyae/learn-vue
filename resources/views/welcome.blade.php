64cecb7e0f560178d12515d6a882446933efd9de
aungzawpyae <aungzawpyaeinf1001mm@gmail.com>
2022-02-11 21:04:32 +0630

    learn vue

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        
+    </head>
+    <body >
+
+       <div id="app">
+        <h1 v-text="message"></h1>
+    </div>
+   
+
+
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+        
+        <script src="{{ asset('js/app.js') }}" ></script>
+        <script scr="/js/app.js"></script>
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script>
+            
+        </script>
+
+    </body>
+</html>
